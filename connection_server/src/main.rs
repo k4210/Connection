af99df4ebdf503df32eff1d846fc44a9bddd56b6
Maciej Mróz <kaziuxmpp@gmail.com>
2019-09-02 23:55:20 +0200

    list of users

--- connection_server/src/main.rs
+++ connection_server/src/main.rs
@@ -59,7 +59,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let console :SafeConsole = Arc::new(Mutex::new(connection_utils::ConsoleBuf::new()));
     let version = env!("CARGO_PKG_VERSION");
     print(&console, format!(">>> Connection version: {}", version));
-    let my_ip: IpAddr = list_ip().unwrap();
+    let my_ip: IpAddr = list_ip().expect("a valid ip");
     let listen_addr = SocketAddr::new(my_ip, connection_utils::SERVER_PORT);
     print(&console, format!(">>> Listen on: {:?}", listen_addr));
     let listener = TcpListener::bind(&listen_addr)?;
@@ -70,21 +70,26 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server = listener.incoming()
         .for_each(move |socket| {
             let (sender, receiver) = futures::sync::mpsc::channel(connection_utils::CHANNEL_BUFF_SIZE);
-            let addr = socket.peer_addr().unwrap();
+            let addr = socket.peer_addr().expect("Socket addr 0");
             let local_state1 = local_state.clone();
-            let list_str = ">>> Other user(s):".to_string() + &list_clients(&state.peers.lock().unwrap());
-            state.peers.lock().unwrap().insert(addr, (sender, None));
+            let handle_users = || -> String { 
+                let mut mg = state.peers.lock().expect("State lock 0");
+                let users = ">>> Other user(s):".to_string() + &list_clients(&mg);
+                mg.insert(addr, (sender, None));
+                users
+            };
+            let list_str = handle_users();
             let con = connection_utils::TextConnection::new(local_console.clone(), receiver, socket
                 , Box::new(move |connection: &connection_utils::TextConnection, msg : String|{
-                let addr = connection.lines.socket.peer_addr().unwrap();
-                let mut mg = local_state1.peers.lock().unwrap();
+                let addr = connection.lines.socket.peer_addr().expect("Socket address 1");
+                let mut mg = local_state1.peers.lock().expect("State lock 1");
                 let mut out_msg: String = String::new();
                 if let Some(val) = mg.get_mut(&addr) {
                     match val {
                         (sender, None) => {
                             out_msg = format!(">>> New user: {} {:?}", &msg, &addr);
                             val.1 = Some(msg);
-                            connection_utils::pass_line(sender, list_str.clone()).unwrap();
+                            connection_utils::pass_line(sender, list_str.clone()).expect("Pass msg1");
                         },
                         (_, Some(name)) => {
                             out_msg = format!("{}: {}", &name, &msg);
@@ -95,7 +100,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 for (addrit, (sender, name)) in &mut (*mg) {
                     if *addrit != addr && *name != None {
-                        connection_utils::pass_line(sender, out_msg.clone()).unwrap();
+                        connection_utils::pass_line(sender, out_msg.clone()).expect("Pass msg2");
                     }
                 }
                 print(&connection.console, out_msg);
@@ -103,12 +108,12 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
             let local_console1 = local_console.clone(); 
             let local_state2 = local_state.clone();
             let match_connection = con.and_then(move |_|{
-                let mut mg = local_state2.peers.lock().unwrap();
+                let mut mg = local_state2.peers.lock().expect("State lock 3");
                 mg.remove(&addr);
                 let list_str = ">>> Connected user(s):".to_string() + &list_clients(&mg);
                 for (_, (sender, name)) in &mut (*mg) {
                     if *name != None {
-                        connection_utils::pass_line(sender, list_str.clone()).unwrap();
+                        connection_utils::pass_line(sender, list_str.clone()).expect("Pass msg3");
                     }
                 }
                 Ok(())
